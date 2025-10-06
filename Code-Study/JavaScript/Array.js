6d8984a76995bb9c6957bf69dd6f697c7d554d21
Anthony <asheadworking@gmail.com>
2025-10-06 14:19:45 -0700

    (feat): Arrays

---
+++ Code-Study/JavaScript/Array.js
@@ -0,0 +1,34 @@
+// This is an example of arrays in JavaScript using fruits as elements
+// this is study page so copy and paste from class
+
+// Array delcaration
+// basic array
+// This does have a order
+const fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits);
+
+// Array Indexing
+// Same starting but includes the number of the index
+const fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Banana
+console.log(fruits[2]); // Cherry
+
+// Arrray leangth
+// This will count the number of elements in the array
+
+const fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits.length); // 3
+console.log(numFruits);
+
+// Array Mutability
+// This will change the value of the array
+const fruits = ["apple", "banana", "cherry"];
+fruits[2] = "strawberry"; // Modifying an element
+fruits[3] = "Kiwi"; // Adding an element
+
+// Push method
+// Adds one or more elements to the end of an array.
+const fruits = ["apple", "banana"];
+fruits.push("orange", "strawberry");
+console.log(fruits)
---
+++ Code-Write/JavaScript/arrays.js
@@ -0,0 +1,7 @@
+
+//This is an example of arrays in JavaScript using fruits as elements
+
+// basic array
+// This does have a order
+let fruits = ["Apple", "Banana", "Cherry"];
+consoloe.log(fruits); // ["Apple", "Banana", "Cherry"]
