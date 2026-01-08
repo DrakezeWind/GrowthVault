c04293bedfd76faad09c51feac5184dc47d66613
DrakezeWind <asheadworking@gmail.com>
2026-01-08 13:57:35 -0800

    Update learning.js

---
+++ Languages/JavaScript/learning.js
@@ -0,0 +1,34 @@
+//keyword
+var variable = "This is a variable";
+console.log(variable);
+
+//let variable
+let letiable = "This is a variable";
+console.log(variable);
+
+//constant
+const constantVariable = "This is a constant variable";
+console.log(constantVariable);
+
+//data types
+//string
+var stringVariable = "This is a string";
+console.log(stringVariable);
+
+//number
+var numberVariable = 42;
+console.log(numberVariable);
+
+const numberOne = 10;
+const numberTwo = 32;
+const sum = numberOne + numberTwo;
+console.log(sum);
+
+//boolean
+var booleanVariable = true;
+console.log(booleanVariable);
+
+//backticks
+var name = "John";
+var greeting = `Hello, ${name}!`;
+console.log(greeting);
